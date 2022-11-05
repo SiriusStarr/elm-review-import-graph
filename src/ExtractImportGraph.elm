67fd7f2a2202f44fdab3da42ae2d6275279622e1
SiriusStarr <2049163+SiriusStarr@users.noreply.github.com>
2022-11-05 10:10:23 -0700

    🚧 Commit everything

--- src/ExtractImportGraph.elm
+++ src/ExtractImportGraph.elm
@@ -6,7 +6,33 @@ module ExtractImportGraph exposing (rule)
 
 -}
 
+import Dict exposing (Dict)
+import Elm.Syntax.Import exposing (Import)
+import Elm.Syntax.ModuleName exposing (ModuleName)
+import Elm.Syntax.Node as Node exposing (Node)
+import Review.Project.Dependency as Dependency exposing (Dependency)
 import Review.Rule as Rule exposing (Rule)
+import Set exposing (Set)
+
+
+type alias ProjectContext =
+    { imports : List ( { name : ModuleName, isSourceModule : Bool }, List { name : ModuleName, isDependency : Bool, isSourceModule : Bool } )
+    , dependencyModules : Set ModuleName
+    , nonSourceModules : Set ModuleName
+    }
+
+
+type alias ModuleContext =
+    { imports : List { name : ModuleName, isDependency : Bool, isSourceModule : Bool }
+    , moduleName : ModuleName
+    , dependencyModules : Set ModuleName
+    , nonSourceModules : Set ModuleName
+    }
+
+
+initContext : ProjectContext
+initContext =
+    { imports = [], dependencyModules = Set.empty, nonSourceModules = Set.empty }
 
 
 {-| Reports... REPLACEME
@@ -45,6 +71,123 @@ elm-review --template SiriusStarr/elm-review-import-graph/example --rules Extrac
 -}
 rule : Rule
 rule =
-    Rule.newModuleRuleSchema "ExtractImportGraph" ()
-        -- Add your visitors
-        |> Rule.fromModuleRuleSchema
+    Rule.newProjectRuleSchema "ExtractImportGraph" initContext
+        |> Rule.withDependenciesProjectVisitor dependencyVisitor
+        |> Rule.withModuleVisitor moduleVisitor
+        |> Rule.withModuleContextUsingContextCreator
+            { fromProjectToModule = fromProjectToModule
+            , fromModuleToProject = fromModuleToProject
+            , foldProjectContexts = foldProjectContexts
+            }
+        |> Rule.withFinalProjectEvaluation finalEvaluationForProject
+        |> Rule.fromProjectRuleSchema
+
+
+dependencyVisitor : Dict String Dependency -> ProjectContext -> ( List never, ProjectContext )
+dependencyVisitor ds context =
+    Dict.values ds
+        |> List.concatMap Dependency.modules
+        |> List.map (String.split "." << .name)
+        |> Set.fromList
+        |> (\dModules -> ( [], { context | dependencyModules = dModules } ))
+
+
+fromProjectToModule : Rule.ContextCreator ProjectContext ModuleContext
+fromProjectToModule =
+    Rule.initContextCreator
+        (\moduleName isSourceFile projectContext ->
+            { imports = []
+            , dependencyModules = projectContext.dependencyModules
+            , moduleName = moduleName
+            , nonSourceModules =
+                if isSourceFile then
+                    projectContext.nonSourceModules
+
+                else
+                    Set.insert moduleName projectContext.nonSourceModules
+            }
+        )
+        |> Rule.withModuleName
+        |> Rule.withIsInSourceDirectories
+
+
+fromModuleToProject : Rule.ContextCreator ModuleContext ProjectContext
+fromModuleToProject =
+    Rule.initContextCreator
+        (\moduleContext ->
+            { imports =
+                [ ( { name = moduleContext.moduleName, isSourceModule = not <| Set.member moduleContext.moduleName moduleContext.nonSourceModules }
+                  , List.reverse moduleContext.imports
+                  )
+                ]
+            , dependencyModules = moduleContext.dependencyModules
+            , nonSourceModules = moduleContext.nonSourceModules
+            }
+        )
+
+
+foldProjectContexts : ProjectContext -> ProjectContext -> ProjectContext
+foldProjectContexts newContext previousContext =
+    { imports = previousContext.imports ++ newContext.imports
+    , dependencyModules = Set.union newContext.dependencyModules previousContext.dependencyModules
+    , nonSourceModules = Set.union newContext.nonSourceModules previousContext.nonSourceModules
+    }
+
+
+moduleVisitor :
+    Rule.ModuleRuleSchema {} ModuleContext
+    -> Rule.ModuleRuleSchema { hasAtLeastOneVisitor : () } ModuleContext
+moduleVisitor schema =
+    schema
+        |> Rule.withImportVisitor importVisitor
+
+
+finalEvaluationForProject : ProjectContext -> List (Rule.Error { useErrorForModule : () })
+finalEvaluationForProject projectContext =
+    let
+        toNode : List String -> String
+        toNode mN =
+            "\"" ++ String.join "." mN ++ "\""
+    in
+    [ Rule.globalError
+        { message = "Import Graph"
+        , details =
+            List.filterMap
+                (\( { name, isSourceModule }, imports ) ->
+                    let
+                        relevantImports =
+                            List.filter (not << .isDependency) imports
+                    in
+                    if List.isEmpty relevantImports || not isSourceModule then
+                        Nothing
+
+                    else
+                        toNode name
+                            ++ " -> {"
+                            ++ String.join " " (List.map (toNode << .name) relevantImports)
+                            ++ "}"
+                            |> Just
+                )
+                projectContext.imports
+                |> (\es -> "digraph {" :: es ++ [ "}" ])
+        }
+    ]
+
+
+importVisitor : Node Import -> ModuleContext -> ( List never, ModuleContext )
+importVisitor imp context =
+    Node.value imp
+        |> .moduleName
+        |> Node.value
+        |> (\n ->
+                ( []
+                , { context
+                    | imports =
+                        { name = n
+                        , isDependency = Set.member n context.dependencyModules
+                        , isSourceModule = not <| Set.member n context.nonSourceModules
+                        }
+                            :: context.imports
+                  }
+                )
+           )
--- tests/ExtractImportGraphTest.elm
+++ tests/ExtractImportGraphTest.elm
@@ -1,6 +1,7 @@
 module ExtractImportGraphTest exposing (all)
 
 import ExtractImportGraph exposing (rule)
+import Review.Project exposing (modules)
 import Review.Test
 import Test exposing (Test, describe, test)
 
@@ -8,17 +9,56 @@ import Test exposing (Test, describe, test)
 all : Test
 all =
     describe "ExtractImportGraph"
-        [ test "should report an error when REPLACEME" <|
+        [ test "should report all imports" <|
             \() ->
-                """module A exposing (..)
-a = 1
+                [ """module A exposing (..)
+
+import B
+import C
+import Dict
+
+foo = 1
 """
-                    |> Review.Test.run rule
-                    |> Review.Test.expectErrors
-                        [ Review.Test.error
-                            { message = "REPLACEME"
-                            , details = [ "REPLACEME" ]
-                            , under = "REPLACEME"
-                            }
+                , """module B exposing (..)
+
+import C
+import Set
+
+bar = 1
+"""
+                , """module C exposing (..)
+
+baz = 1
+"""
+                ]
+                    |> Review.Test.runOnModules rule
+                    |> Review.Test.expectGlobalErrors
+                        [ importGraph
+                            [ ( [ "A" ]
+                              , [ [ "B" ], [ "C" ] ]
+                              )
+                            , ( [ "B" ]
+                              , [ [ "C" ] ]
+                              )
+                            ]
                         ]
         ]
+
+
+importGraph : List ( List String, List (List String) ) -> { message : String, details : List String }
+importGraph modules =
+    { message = "Import Graph"
+    , details =
+        "digraph {"
+            :: List.map
+                (\( moduleName, imports ) ->
+                    "\""
+                        ++ String.join "." moduleName
+                        ++ "\""
+                        ++ " -> {"
+                        ++ String.join " " (List.map (\s -> "\"" ++ String.join "." s ++ "\"") imports)
+                        ++ "}"
+                )
+                modules
+            ++ [ "}" ]
+    }
